76d4e684e9e09ded9107240847c5b567cdc00afc
Isaac Sánchez <isaacsancheza@outlook.com>
2022-07-07 12:04:00 -0500

    Add command-line options

--- ptz.py
+++ ptz.py
@@ -144,3 +144,49 @@ class PTZ:
             return response.text
 
         assert response.text.strip() == 'OK', response.text
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser('ptz')
+    subparsers = parser.add_subparsers(dest='command')
+
+    get_parser = subparsers.add_parser('get')
+    set_parser = subparsers.add_parser('set')
+
+    get_subparsers = get_parser.add_subparsers(dest='get_command')
+    set_subparsers = set_parser.add_subparsers(dest='set_command')
+
+    get_position_parser = get_subparsers.add_parser('position')
+    set_position_parser = set_subparsers.add_parser('position')
+
+    set_position_parser.add_argument('x', type=float, help='X Position')
+    set_position_parser.add_argument('y', type=float, help='Y Position')
+    set_position_parser.add_argument('z', type=float, help='Zoom [0 - 128.0]')
+    set_position_parser.add_argument('s', type=int, help='Speed [1-8]')
+
+    for required_envrionment_variable in REQUIRED_ENVIRONMENT_VARIABLES:
+        if required_envrionment_variable not in environ:
+            parser.error(f'environment variable {required_envrionment_variable} not set')
+
+    parser.set_defaults(command='', get_command='', set_command='')
+    args = parser.parse_args()
+
+    ptz = PTZ()
+    command = args.command
+    get_command = args.get_command
+    set_command = args.set_command
+
+    if command == 'get':
+        if get_command == 'position':
+            print(ptz.position)
+        else:
+            get_parser.print_help()
+    elif command == 'set':
+        if set_command == 'position':
+            ptz.go_to(args.x, args.y, args.z, args.s)
+        else:
+            set_parser.print_help()
+    else:
+        parser.print_help()
